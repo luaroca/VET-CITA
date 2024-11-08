0bae21eafb4ebe0b2daa66a86289d2e1dd751d79
Emelin Rodriguez <eelvirarodriguez@unicesar.edu.co>
2024-11-07 22:56:41 -0500

    Creacion del Bot

---
+++ GUI/Bot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace GUI
+{
+    public class Bot
+    {
+        public static ITelegramBotClient _botClient;
+        
+        public Bot() 
+        {
+            _botClient = new TelegramBotClient("7763752688:AAEi2Z1B6Sc0g-uJ30ILEIdNBI65SLW32fE");
+        }
+
+        public string NombreId()
+        { 
+            var me = _botClient.GetMe().Result;
+
+
+           return ($"Hi, I am {me.Id} and my name is: {me.FirstName} ");      
+        }
+
+
+            
+    }
+}
--- GUI/Program.cs
+++ GUI/Program.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Telegram.Bot;
 
 namespace GUI
 {
     internal static class Program
     {
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
+
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
